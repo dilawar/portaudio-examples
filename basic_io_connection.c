378bbb59eb82aca7bea39b3fe51444f78cba2180
Dilawar Singh <dilawar.s.rajput@gmail.com>
2021-03-31 17:07:22 +0530

    some formatting changes.

--- basic_io_connection.c
+++ basic_io_connection.c
@@ -1,9 +1,9 @@
+#include <portaudio.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <portaudio.h>
 
 static PaError err;
-static PaStream *stream = NULL;
+static PaStream* stream = NULL;
 
 /*
  * FUNCTION: PaErrorTest()
@@ -13,30 +13,35 @@ static PaStream *stream = NULL;
  * Returns:	return on stderr the nature of the error in a meaningful way.
  *
  */
-void PaErrorTest(PaError error) {
-	if (error != paNoError) {
-		// using Pa_Terminate is crucial to avoid resource leaks
-		Pa_Terminate();
-		fprintf(stderr, "Error: %s\n", Pa_GetErrorText(error));
-		exit(EXIT_FAILURE);
-	}
+void PaErrorTest(PaError error)
+{
+    if (error != paNoError) {
+        // using Pa_Terminate is crucial to avoid resource leaks
+        Pa_Terminate();
+        fprintf(stderr, "Error: %s\n", Pa_GetErrorText(error));
+        exit(EXIT_FAILURE);
+    }
 }
 
-static void PaInit() {
-	static int initialized;
+static void
+PaInit()
+{
+    static int initialized;
 
-	if(!initialized) {
-		err = Pa_Initialize();
+    if (!initialized) {
+        err = Pa_Initialize();
 
-		PaErrorTest(err);
-		initialized = 1;
-	}
+        PaErrorTest(err);
+        initialized = 1;
+    }
 }
 
-static void PaClose(PaStream *stream) {
-	err = Pa_StopStream(stream);
-	PaErrorTest(err);
-	Pa_Terminate();
+static void
+PaClose(PaStream* stream)
+{
+    err = Pa_StopStream(stream);
+    PaErrorTest(err);
+    Pa_Terminate();
 }
 
 /*
@@ -59,83 +64,84 @@ static void PaClose(PaStream *stream) {
  *		stream to continue running.
  *
  */
-int g_num_no_imputs = 0;
-int PlayrecCallback(const void *input_buffer,
-			   void *output_buffer,
-			   unsigned long frames_per_buffer,
-			   const PaStreamCallbackTimeInfo* time_info,
-			   PaStreamCallbackFlags status_flags,
-			   void *user_data) {
-	float *out = (float *) output_buffer;
-	const float *in = (const float *)input_buffer;
-	unsigned int i;
+int PlayrecCallback(const void* input_buffer,
+    void* output_buffer,
+    unsigned long frames_per_buffer,
+    const PaStreamCallbackTimeInfo* time_info,
+    PaStreamCallbackFlags status_flags,
+    void* user_data)
+{
+    float* out = (float*)output_buffer;
+    const float* in = (const float*)input_buffer;
+    unsigned int i;
 
-	(void) time_info;
-	(void) status_flags;
-	(void) user_data;
+    (void)time_info;
+    (void)status_flags;
+    (void)user_data;
 
-	if(input_buffer != NULL) {
-		for(i=0; i<frames_per_buffer; i++) {
-			*out++ = *in++;
-			*out++ = *in++;
-		}
-	}
-	return paContinue;
+    if (input_buffer != NULL) {
+        for (i = 0; i < frames_per_buffer; i++) {
+            *out++ = *in++;
+            *out++ = *in++;
+        }
+    }
+    return paContinue;
 }
 
-int main (int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-	// Definition of variables we need
-	PaStreamParameters in_params, out_params; // structures for I/Os configuration
-	int num_channels = 2; // 2 channels (stereo)
-	int samplerate = 44100; // sample rate
-	int device_id = 0; // sound card device ID (is dependent of your system
-	int frames_per_buffer = 256; // audio buffer for portaudio
-	char sample_format = paFloat32; // type of sample format
+    // Definition of variables we need
+    PaStreamParameters in_params, out_params; // structures for I/Os configuration
+    int num_channels = 2; // 1 channels (mono), 2 (stereo)
+    int samplerate = 44100; // sample rate (44100 is default on most devices)
+    int frames_per_buffer = 128; // audio buffer for portaudio
+    char sample_format = paFloat32; // type of sample format
 
+    // Initialization of portaudio
+    Pa_Initialize();
 
-	// Initialization of portaudio
-	PaInit();
+    // INPUTS
+    in_params.device = Pa_GetDefaultInputDevice();
+    in_params.channelCount = num_channels;
+    in_params.sampleFormat = sample_format;
+    in_params.suggestedLatency = Pa_GetDeviceInfo(in_params.device)->defaultLowInputLatency;
+    in_params.hostApiSpecificStreamInfo = NULL;
 
-	// INPUTS
-	in_params.device = device_id;
-	in_params.channelCount = num_channels;
-	in_params.sampleFormat = sample_format;
-	in_params.suggestedLatency =
-		Pa_GetDeviceInfo(in_params.device)->defaultLowInputLatency;
-	in_params.hostApiSpecificStreamInfo = NULL;
-	// OUTPUTS
-	out_params.device = device_id + 1;
-	out_params.channelCount = num_channels;
-	out_params.sampleFormat = sample_format;
-	out_params.suggestedLatency =
-		Pa_GetDeviceInfo(out_params.device)->defaultLowOutputLatency;
-	out_params.hostApiSpecificStreamInfo = NULL;
+    // OUTPUTS
+    out_params.device = Pa_GetDefaultOutputDevice();
+    out_params.channelCount = num_channels;
+    out_params.sampleFormat = sample_format;
+    out_params.suggestedLatency = Pa_GetDeviceInfo(out_params.device)->defaultLowOutputLatency;
+    out_params.hostApiSpecificStreamInfo = NULL;
 
-	/* Configuration of stream
-	 *
-	 * We call the callback we'll need to avoid blocking I/Os
-	 */
-	err = Pa_OpenStream(
-			   &stream,
-			   &in_params,
-			   &out_params,
-			   samplerate,
-			   frames_per_buffer,
-			   paNoFlag, /* portaudio will clip for us */
-			   PlayrecCallback,
-			   NULL);
-	PaErrorTest(err);
+    printf("[INFO] Input device %d, output device %d\n",
+        in_params.device,
+        out_params.device);
 
-	// Starting of the stream
-	err = Pa_StartStream(stream);
-	PaErrorTest(err);
+    /* Configuration of stream
+   *
+   * We call the callback we'll need to avoid blocking I/Os
+   */
+    err = Pa_OpenStream(&stream,
+        &in_params,
+        &out_params,
+        samplerate,
+        frames_per_buffer,
+        paNoFlag, /* portaudio will clip for us */
+        PlayrecCallback,
+        NULL);
+    PaErrorTest(err);
 
-	// close the program whenever you want
-	printf("Hit ENTER to stop program.\n");
-	getchar();
-	// stop the stream
-	PaClose(stream);
+    // Starting of the stream
+    err = Pa_StartStream(stream);
+    PaErrorTest(err);
 
-	return EXIT_SUCCESS;
+    // close the program whenever you want
+    printf("Hit ENTER to stop program.\n");
+    getchar();
+
+    // stop the stream
+    PaClose(stream);
+
+    return EXIT_SUCCESS;
 }
